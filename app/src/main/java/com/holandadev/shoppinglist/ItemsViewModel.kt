ebb781a421cd2de2d77ee7d3db8d00de20ffac5f
Joao Holanda <joaocmdt505@gmail.com>
2024-08-19 16:05:24 -0300

    feat: Implement view model refactoring to use database

--- app/src/main/java/com/holandadev/shoppinglist/ItemsViewModel.kt
+++ app/src/main/java/com/holandadev/shoppinglist/ItemsViewModel.kt
@@ -2,22 +2,30 @@ package com.holandadev.shoppinglist
 
 import androidx.lifecycle.MutableLiveData
 import androidx.lifecycle.ViewModel
+import androidx.lifecycle.viewModelScope
 import com.holandadev.shoppinglist.data.ItemsDatabase
+import com.holandadev.shoppinglist.data.toModel
+import kotlinx.coroutines.Dispatchers
+import kotlinx.coroutines.launch
 
-class ItemsViewModel(
-    private val database: ItemsDatabase
-): ViewModel() {
-    private var items = mutableListOf<ItemModel>()
+class ItemsViewModel(private val database: ItemsDatabase): ViewModel() {
+
+    init {
+        viewModelScope.launch(Dispatchers.IO) {
+            fetchAll()
+        }
+    }
+
     val itemsLiveData = MutableLiveData<List<ItemModel>>()
 
     fun addItem(name: String) {
-        val item = ItemModel(name = name, onRemove = ::removeItem)
-        items.add(item)
-        itemsLiveData.value = items
+
     }
 
-    private fun removeItem(item: ItemModel) {
-        items.remove(item)
-        itemsLiveData.value = items
+    private suspend fun fetchAll() {
+        val result = database.itemsDao().getAll().map {
+            it.toModel(onRemove = {})
+        }
+        itemsLiveData.postValue(result)
     }
 }
