c80c3f15156dff45b316b371a479046cab43ea3f
Joao Holanda <joaocmdt505@gmail.com>
2024-08-18 21:35:04 -0300

    feat: Implement connection of viewmodel with activity

--- app/src/main/java/com/holandadev/shoppinglist/MainActivity.kt
+++ app/src/main/java/com/holandadev/shoppinglist/MainActivity.kt
@@ -4,12 +4,16 @@ import android.os.Bundle
 import android.widget.Button
 import android.widget.EditText
 import androidx.activity.enableEdgeToEdge
+import androidx.activity.viewModels
 import androidx.appcompat.app.AppCompatActivity
 import androidx.core.view.ViewCompat
 import androidx.core.view.WindowInsetsCompat
 import androidx.recyclerview.widget.RecyclerView
 
 class MainActivity : AppCompatActivity() {
+
+    val viewModel: ItemsViewModel by viewModels()
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
@@ -26,15 +30,13 @@ class MainActivity : AppCompatActivity() {
                 return@setOnClickListener
             }
 
-            val item = ItemModel(
-                name = editText.text.toString(),
-                onRemove = {
-                    itemsAdapter.removeItem(it)
-                }
-            )
+            viewModel.addItem(editText.text.toString())
 
-            itemsAdapter.addItem(item)
             editText.text.clear()
+        }
+
+        viewModel.itemsLiveData.observe(this) { items ->
+            itemsAdapter.updateItems(items)
         }
     }
 }
